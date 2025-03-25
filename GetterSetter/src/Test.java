4a5c5f5a8b6ecab1069735f60728f22534724fab
Katharine007 <124885024+Katharine007@users.noreply.github.com>
2025-03-25 12:08:36 +0530

    commit 1

---
+++ GetterSetter/src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+private String a = "Hello";
+
+ public String getA() {
+		return a;
+	}
+
+
+
+	@Override
+public String toString() {
+	return "Assignment1 [a=" + a + "]";
+}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+
+}
